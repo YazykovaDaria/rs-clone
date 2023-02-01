12a6e759b57bf86899fb120ede947b64b830f800
YazykovaDaria <yazykovadasva@gmail.com>
2023-02-01 12:58:11 +0300

    feat: add vitest

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -21,5 +21,9 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 0,
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/*.test.tsx', '**/*.spec.tsx'] },
+    ],
   },
 };
