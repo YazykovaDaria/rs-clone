e0dc1d4590dcc803a0852a4bd24f248a97325389
YazykovaDaria <yazykovadasva@gmail.com>
2023-02-04 22:33:14 +0300

    feat: sidebar

--- frontend/src/features/sidebar/SideBar.tsx
+++ frontend/src/features/sidebar/SideBar.tsx
@@ -1,11 +1,20 @@
+import './style.css';
 import { Link } from 'react-router-dom';
+import Nav from './Nav';
+import { ReactComponent as Logo } from '../../../public/icon/icons8-twitter.svg';
 
 function SideBar() {
   return (
-    <aside>
-      <Link to="/">Main</Link>
-      <Link to="/profile">Profile</Link>
-      <button type="button">tweet</button>
+    <aside className="flex flex-col justify-between p-3 sm:h-screen">
+      <div className="flex sm:flex-col justify-between gap-5">
+        <Link to="/" className="hover:bg-slate-300 hover:rounded-full">
+          <Logo />
+        </Link>
+
+        <Nav />
+
+        <div className="aside-bnt">Tweet</div>
+      </div>
     </aside>
   );
 }
