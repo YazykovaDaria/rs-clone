3fd30d7f6ba5b9ce4fc75f3223a445d599b9326c
YazykovaDaria <yazykovadasva@gmail.com>
2023-02-06 19:03:41 +0300

    feat: userWidget

--- frontend/src/features/userSidebarWidget/UserWitget.tsx
+++ frontend/src/features/userSidebarWidget/UserWitget.tsx
@@ -1,47 +1,53 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 import './style.css';
 import { useState } from 'react';
+import ButtonCloseSvg from '../../shared/ButtonCloseSvg/ButtonCloswSvg';
+import Modal from '../../shared/modal/Modal';
 
 function UserWidget() {
   const [isOpenPopup, setPopup] = useState(false);
+  const [isOpenModal, setModal] = useState(false);
 
-  const closePopup = () => setPopup(false);
+  const openModal = (): void => setModal(true);
 
-  const togglePopup = () => setPopup(!isOpenPopup);
+  const closeModal = (): void => setModal(false);
+
+  const closePopup = (): void => setPopup(false);
+
+  const togglePopup = (): void => setPopup(!isOpenPopup);
 
   return (
-    <div className="flex flex-col-reverse sm:flex-col gap-2">
+    <div className="flex flex-col-reverse sm:flex-col gap-2 cursor-pointer sm:w-11/12">
       <div className={isOpenPopup ? 'user-popup' : 'hidden'}>
-        <svg
-          onClick={closePopup}
-          className="absolute right-1 top-1 w-5 h-5 hover:bg-zinc-400 hover:rounded-full"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-
+        <ButtonCloseSvg close={closePopup} />
         <div className="hover:text-sky-600 hover:font-bold">
-          Change language
+          <button
+            type="button"
+            onClick={() => {
+              openModal();
+              closePopup();
+            }}
+            className="p-0"
+          >
+            Switch language
+          </button>
+          <Modal isOpen={isOpenModal} onClose={closeModal}>
+            lang
+          </Modal>
         </div>
-        <div className="hover:text-sky-600 hover:font-bold">Logout</div>
+
+        <div className="hover:text-sky-600 hover:font-bold cursor-pointer">
+          Logout
+        </div>
       </div>
 
-      <div className="user-aside" onClick={togglePopup}>
+      <button type="button" className="user-aside" onClick={togglePopup}>
         <div className="w-10 h-10 rounded-full border-solid border-2 border-cyan-500 " />
         <div className="flex-col hidden md:flex">
           <span>User name</span>
           <span className="text-slate-300">user_nickname</span>
         </div>
         <span className="hidden sm:inline-block text-lg">...</span>
-      </div>
+      </button>
     </div>
   );
 }
