78608b6c2a6e834de632300e1a1b0a5d18b2a4e7
YazykovaDaria <yazykovadasva@gmail.com>
2023-02-06 19:07:01 +0300

    refactor:  change aside

--- frontend/src/features/sidebar/SideBar.tsx
+++ frontend/src/features/sidebar/SideBar.tsx
@@ -2,18 +2,17 @@ import './style.css';
 import { Link } from 'react-router-dom';
 import Nav from './Nav';
 import UserWidget from '../userSidebarWidget/UserWitget';
-import { ReactComponent as Logo } from '../../../public/icon/icons8-twitter.svg';
+import { ReactComponent as Logo } from '../../shared/assets/icons/icons8-twitter.svg';
 
 function SideBar() {
   return (
-    <aside className="flex sm:flex-col justify-between p-3 sm:h-screen">
+    <aside className="flex sm:flex-col justify-between items-center p-3 sm:h-screen">
       <div className="flex sm:flex-col justify-between gap-5">
-        <Link to="/" className="hover:bg-slate-300 hover:rounded-full">
-          <Logo />
+        <Link to="/">
+          <Logo className="hover:fill-cyan-500 fill-cyan-300" />
         </Link>
 
         <Nav />
-
         <div className="aside-bnt">Tweet</div>
       </div>
       <UserWidget />
