52ba7f4c4eae013d22a254390a08d9b0a3347230
YazykovaDaria <yazykovadasva@gmail.com>
2023-02-06 18:43:37 +0300

    feat: svg

--- frontend/src/features/sidebar/Nav.tsx
+++ frontend/src/features/sidebar/Nav.tsx
@@ -1,64 +1,27 @@
 import { NavLink } from 'react-router-dom';
 import './style.css';
+import { ReactComponent as Chat } from '../../shared/assets/icons/chat.svg';
+import { ReactComponent as Profile } from '../../shared/assets/icons/profile.svg';
+import { ReactComponent as Home } from '../../shared/assets/icons/home.svg';
 
 function Nav() {
   return (
     <ul className="flex gap-4 sm:gap-3 flex-row justify-between sm:flex-col">
       <li>
         <NavLink to="/" className="aside-link">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-            />
-          </svg>
+          <Home />
           <p className="hidden sm:block text-xl">Home</p>
         </NavLink>
       </li>
       <li>
         <NavLink to="/profile" className="aside-link">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-            />
-          </svg>
+          <Profile />
           <p className="hidden sm:block text-xl">Profile</p>
         </NavLink>
       </li>
       <li>
         <NavLink to="/chat" className="aside-link">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
-            />
-          </svg>
-
+          <Chat />
           <p className="hidden sm:block text-xl">Messages</p>
         </NavLink>
       </li>
