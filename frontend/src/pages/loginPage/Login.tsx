d665cfdb24d28c4b7c157d2265d84d8506ecabf8
YazykovaDaria <yazykovadasva@gmail.com>
2023-02-10 23:17:49 +0300

    feat: login form

--- frontend/src/pages/loginPage/Login.tsx
+++ frontend/src/pages/loginPage/Login.tsx
@@ -1,6 +1,26 @@
+import { useTranslation } from 'react-i18next';
+import { NavLink } from 'react-router-dom';
+import FormHeader from '../../shared/IU/FormHeader/FormHeader';
+import LoginForm from '../../entities/LoginForm/LoginForm';
+
 function Login() {
+  const { t } = useTranslation();
+
   return (
-    <div className="max-w-screen-xl mx-auto justify-between">Login page</div>
+    <div className="relative">
+      <div className="p-2 max-w-2xl m-auto text-base flex flex-col gap-5">
+        <FormHeader />
+        <h2 className="text-3xl text-center">{t('authLoginForm.logIn')}</h2>
+        <LoginForm />
+
+        <div className="flex justify-between text-xl text-blue-350">
+          <span>{t('authLoginForm.notHave')}</span>
+          <NavLink to="/auth" className="hover:text-blue-600">
+            {t('authLoginForm.registration')}
+          </NavLink>
+        </div>
+      </div>
+    </div>
   );
 }
 
