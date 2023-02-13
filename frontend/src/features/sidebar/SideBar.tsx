2841903d7eede9be17d6559c00cf716a0f6dea8a
YazykovaDaria <yazykovadasva@gmail.com>
2023-02-13 21:22:12 +0300

    feat: modal for write twit

--- frontend/src/features/sidebar/SideBar.tsx
+++ frontend/src/features/sidebar/SideBar.tsx
@@ -1,12 +1,16 @@
 import './style.css';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
 import Nav from './Nav';
 import UserWidget from '../userSidebarWidget/UserWitget';
 import { ReactComponent as Logo } from '../../shared/assets/icons/icons8-twitter.svg';
+import Modal from '../../shared/IU/modal/Modal';
+import TwitCreator from '../twit-creator/Twit-creator';
 
 function SideBar() {
   const { t } = useTranslation();
+  const [isOpenModal, setModal] = useState(false);
   return (
     <aside className="flex flex-col justify-between sm:p-3 h-screen sticky top-0 left-0">
       <div className="flex flex-col justify-between gap-5">
@@ -15,7 +19,25 @@ function SideBar() {
         </Link>
 
         <Nav />
-        <div className="aside-bnt">{t('tweet')}</div>
+
+        <Modal isOpen={isOpenModal} onClose={() => setModal(false)}>
+          <TwitCreator />
+        </Modal>
+
+        <button
+          type="button"
+          onClick={() => setModal(true)}
+          className="aside-bnt bg-sky-400 "
+        >
+          {t('tweet')}
+        </button>
+        <button
+          type="button"
+          className="rounded-full m-2 bg-sky-400 sm:hidden"
+          onClick={() => setModal(true)}
+        >
+          +
+        </button>
       </div>
       <UserWidget />
     </aside>
