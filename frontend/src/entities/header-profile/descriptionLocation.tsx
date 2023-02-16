80e0e81fbaa9f767e873587685402933272587dd
Vadim Shershen <vadim.shershen.97@mail.ru>
2023-02-17 00:24:52 +0300

    refactor: change svg on ReactComponent

--- frontend/src/entities/header-profile/descriptionLocation.tsx
+++ frontend/src/entities/header-profile/descriptionLocation.tsx
@@ -1,3 +1,5 @@
+import { ReactComponent as LocationIcon } from '../../shared/assets/icons/location-icon.svg';
+
 export default function DescriptionLocation({
   location,
 }: {
@@ -5,24 +7,7 @@ export default function DescriptionLocation({
 }) {
   return (
     <div className="flex flex-row flex-nowrap">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        className="w-5 h5 stroke-gray-350"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-        />
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-        />
-      </svg>
+      <LocationIcon />
       <span className="text-gray-350 ml-1 mr-5">{location}</span>
     </div>
   );
