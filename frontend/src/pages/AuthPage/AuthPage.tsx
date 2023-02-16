3224d07a8dc5e3f9e61b48b721e8fb4d128eb4f3
YazykovaDaria <yazykovadasva@gmail.com>
2023-02-16 13:51:53 +0300

    refactor: change save user data

--- frontend/src/pages/AuthPage/AuthPage.tsx
+++ frontend/src/pages/AuthPage/AuthPage.tsx
@@ -1,9 +1,13 @@
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 import AuhtForm from '../../features/AuthForm/AuthForm';
 import FormHeader from '../../shared/IU/FormHeader/FormHeader';
 
 function Auth() {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const goBack = () => navigate(-1);
 
   return (
     <div className="p-2 max-w-3xl m-auto text-base flex flex-col gap-5 mt-4">
@@ -12,6 +16,13 @@ function Auth() {
         {t('authLoginForm.createAccount')}
       </h2>
       <AuhtForm />
+      <div className="p-1">
+        <button
+          className="text-lg text-blue-800 hover:text-blue-350"
+          type="button"
+          onClick={goBack}
+        >{`⟵ ${t('goBack')}`}</button>
+      </div>
     </div>
   );
 }
